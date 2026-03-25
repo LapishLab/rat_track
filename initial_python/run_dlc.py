b0b64e4acd65eae58ebdb39648adb6b9d8e658b8
Brain8 (David Swygart) <davidswygart@gmail.com>
2026-03-25 15:27:02 -0400

    run_dlc operates per video

--- initial_python/run_dlc.py
+++ initial_python/run_dlc.py
@@ -1,72 +1,77 @@
 #!/home/lapishla/miniconda3/envs/dlc/bin/python3
-import deeplabcut
-import os
-import glob
+from deeplabcut import analyze_videos, filterpredictions, create_labeled_video
 import sys
+from pathlib import Path
 
-def main(job_folder, shuffle=3):
-    config = "/home/lapishla/Desktop/dlc/networks/2CAP-Pi/config.yaml" # TODO make not hard coded. Put model in repository?
-    videos = job_folder
-    destfolder = os.path.join(job_folder, f"dlc_results_shuffle{shuffle}")
-    os.makedirs(destfolder, exist_ok=True)
+def run_dlc(
+    video_folder,
+    project_root = None,
+    create_video = True,
+    network_path = "/research/lapishla/dlc/networks/2CAP-Pi",
+    shuffle=3,
+    ):
 
+    # Parse arguments
+    video_folder = Path(video_folder)
+    network_path = Path(network_path)
+    if project_root is None:
+        project_root = video_folder.parent
+    else:
+        project_root = Path(project_root)
     shuffle = int(shuffle)
 
-    deeplabcut.analyze_videos(
-        config=config,
-        videos=videos,
-        destfolder=destfolder,
-        save_as_csv=True,
-        shuffle=shuffle
-    )
+    # Prepare paths
+    output_root = project_root / f'dlc-results_{network_path.name}_shuff{shuffle}'
+    config =  network_path / 'config.yaml'
 
-    deeplabcut.filterpredictions(
-        config=config,
-        video=videos,
-        destfolder=destfolder,
-        save_as_csv=True,
-        shuffle=shuffle
-    )
+    for v in video_folder.glob('*.mp4'):
+        output_folder = output_root / v.stem
+        output_folder.mkdir(parents=True, exist_ok=True)
+        
+        if contains_files(output_folder, ['*filtered.csv','*filtered.h5','*meta.pickle']):
+            print(f'DLC output already present for {v} at {output_folder}. Skipping')
+        else:
+            print(f'Starting DLC analysis of {v}')
+            analyze_videos(
+                config=config,
+                videos=[str(v)],
+                destfolder=str(output_folder),
+                save_as_csv=True,
+                shuffle=shuffle
+            )
+            filterpredictions(
+                config=config,
+                video=[str(v)],
+                destfolder=str(output_folder),
+                save_as_csv=True,
+                shuffle=shuffle
+            )
 
-    deeplabcut.plot_trajectories(
-        config=config,
-        videos=videos,
-        destfolder=destfolder,
-        filtered=True,
-        pcutoff = None,
-        shuffle=shuffle
-    )
+        if create_video:
+            if contains_files(output_folder, ['*labeled.mp4']):
+                print(f'Labeled video already present for {v} at {output_folder}. Skipping')
+            else:
+                print(f'Generating labeled video for {v}')
+                create_labeled_video(
+                    config=config,
+                    videos=[str(v)],
+                    destfolder=str(output_folder),
+                    filtered=True,
+                    trailpoints = 1,
+                    pcutoff = 0,
+                    overwrite = True,
+                    dotsize=1,
+                    draw_skeleton=False,
+                    shuffle=shuffle
+                )
 
 
-    deeplabcut.create_labeled_video(
-        config=config,
-        videos=videos,
-        destfolder=destfolder,
-        filtered=True,
-        trailpoints = 1,
-        pcutoff = 0,
-        overwrite = True,
-        dotsize=1,
-        draw_skeleton=False,
-        shuffle=shuffle
-    )
-    organize_results(destfolder)
-
-def organize_results(destfolder):
-    move_files_to_subdirectory(destfolder, "labeled_video",".mp4")
-    move_files_to_subdirectory(destfolder, "xy_filtered","_filtered.csv")
-    move_files_to_subdirectory(destfolder, "skeleton","_skeleton.csv")
-    move_files_to_subdirectory(destfolder, "xy_raw",".csv")
-    move_files_to_subdirectory(destfolder, "h5",".h5")
-    move_files_to_subdirectory(destfolder, "pickle",".pickle")
-
-def move_files_to_subdirectory(current_dir, sub_name, file_end):
-    sub_dir = os.path.join(current_dir, sub_name)
-    os.makedirs(sub_dir, exist_ok=True)
-    for f in glob.glob(os.path.join(current_dir, f"*{file_end}")):
-        os.rename(f, os.path.join(sub_dir, os.path.basename(f)))
+def contains_files(folder, required):
+    folder = Path(folder)
+    in_folder = [any(folder.glob(r)) for r in required]
+    return all(in_folder)
 
 
 if __name__ == "__main__":
-    job_folder = sys.argv[1]
-    main(job_folder)
+    video_folder = sys.argv[1]
+    run_dlc(video_folder)
