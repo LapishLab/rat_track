926ae8cca058c98c944c799d50c2acf0e41ba6fc
Brain8 (David Swygart) <davidswygart@gmail.com>
2026-03-31 17:51:29 -0400

    make single tracking load its own function

--- matlab_utilities/loading/get_all_tracking.m
+++ matlab_utilities/loading/get_all_tracking.m
@@ -20,25 +20,3 @@ function tracking = get_all_tracking(job_folder, id)
     %oe_sync.Properties.VariableNames = "time_" + oe_sync.Properties.VariableNames;
     %tracking = cat(2, oe_sync, tracking);
 end
-
-function tracking = load_tracking_csv(csv_path, header_lines)
-    % allow for wildcards
-    d = dir(csv_path);
-    csv_path = [d.folder filesep d.name];
-
-    % load header
-    header_rows = cell(max(header_lines), 1);
-    fid = fopen(csv_path, 'r');
-    for i=1:max(header_lines)
-        header_rows{i} = strsplit(fgets(fid), ',');
-    end
-    fclose(fid); % Close the file
-
-    header_rows = header_rows(header_lines);
-    header = string(header_rows{1}) + "_" +  string(header_rows{2});
-    header(1) = "frame";
-
-    % load the data
-    tracking = readtable(csv_path, Delimiter=',', NumHeaderLines=max(header_lines));
-    tracking.Properties.VariableNames = header;
-end
